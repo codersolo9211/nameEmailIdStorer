d71daee661e26d19f2cebb51b8da814a2ea4037b
codersolo9211 <141203987+codersolo9211@users.noreply.github.com>
2024-10-28 08:43:49 +0530

    name and email id storer

---
+++ project-5/src/components/NavBar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <div className='h-[3.75rem] bg-white my-4 rounded-lg items-center justify-center flex gap-2 text-xl font-medium'>
+      
+        <img src='./images/logos_firebase.png' />
+        <h1>Firebase Contact App</h1>
+      
+    </div>
+  )
+}
+
+export default NavBar
+

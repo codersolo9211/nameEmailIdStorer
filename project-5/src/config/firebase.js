76d4549a261cda54ad6afbdaed37dfe285e3208f
codersolo9211 <141203987+codersolo9211@users.noreply.github.com>
2024-10-28 11:56:31 +0530

    deployment

--- project-5/src/config/firebase.js
+++ project-5/src/config/firebase.js
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 import  { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyAVSSSKOUqGJgHoCcNTrmyjnb0zMbTMoYw",
+  authDomain: "vite-connect.firebaseapp.com",
+  projectId: "vite-connect",
+  storageBucket: "vite-connect.appspot.com",
+  messagingSenderId: "599733676940",
+  appId: "1:599733676940:web:22493d6ce1fa5c3000c4a9"
 };
 
 
